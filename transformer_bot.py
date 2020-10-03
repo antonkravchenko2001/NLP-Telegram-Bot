fa7904ffb98646205c5c95af480a0e5645fbde07
anton <antonio57banderas@gmail.com>
2020-10-03 15:44:13 +0300

    first commit

--- transformer_bot.py
+++ transformer_bot.py
@@ -1,23 +1,18 @@
-from model import predict, input_dict
-import telebot
-from flask import Flask, request
 import os
-
+import telebot
+from model import predict, input_dict
+from flask import Flask, request
 
 server = Flask(__name__)
-PORT = int(os.environ.get('PORT', 5000))
+TOKEN = "1214344625:AAGtZ34OgYUOQSMLgwC9tiGkqhJIsJJ-1Bg"
+bot = telebot.TeleBot(token=TOKEN)
 
 
 def check_validity(message):
-    m = message.text.split()
-    for word in m:
-        if word not in input_dict.keys():
+    for word in message.split():
+        if word not in input_dict:
             return False
-    return True
-
-
-bot_token = '1214344625:AAGtZ34OgYUOQSMLgwC9tiGkqhJIsJJ-1Bg'
-bot = telebot.TeleBot(token=bot_token)
+        return True
 
 
 @bot.message_handler(commands=['start'])
@@ -25,26 +20,40 @@ def send_welcome(message):
     bot.reply_to(message, 'Welcome!')
 
 
-@bot.message_handler(func=lambda mess: check_validity(mess) == True)
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Type something')
+
+
+@bot.message_handler(func=lambda msg: check_validity(msg.text))
 def reply(message):
-    bot.reply_to(message, predict(message.text))
+    prediction = predict(message.text)
+    bot.reply_to(message, prediction)
 
 
-@server.route('/' + bot_token, methods=['POST'])
-def get_message():
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!" "200"
+    return "!", 200
 
 
-@server.route('/')
+@server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://telegramtransformer.herokuapp.com/' + bot_token)
-    return "!" "200"
+    bot.set_webhook(url='https://telegram-transformer-bot.com/' + TOKEN)
+    return "!", 200
 
 
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 
+
+"""
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception:
+        time.sleep(15)
+"""
